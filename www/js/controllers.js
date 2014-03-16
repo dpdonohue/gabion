24b0fd24ea2826b10a0bf9f8ba804a3a56a92e4f
dpdonohue <dd@daviddonohue.com>
2014-03-15 23:03:41 -0400

    voice recognition works, adding testing aspect and use of csv for translations

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,9 @@ angular.module('gabi.controllers', [])
 .controller('TranslateCtrl', function($scope, AndroidSpeechRecognizer, Settings, Lookup) {
     $scope.settings = Settings;
     $scope.recognizedSpeech = "";
-        $scope.lookup = Lookup;
+    $scope.lookup = Lookup;
+    var termIndex = 0;
+    var currentTerm = [];
 
     $scope.receiveRecognizedSpeech = function(text) {
         $scope.recognizedSpeech = text;
@@ -13,7 +15,46 @@ angular.module('gabi.controllers', [])
     $scope.record = function() {
         AndroidSpeechRecognizer.recognizeSpeech($scope.receiveRecognizedSpeech, Settings.targetLanguage);
     }
-        alert('lookup=' + Lookup);
+
+    $scope.nextTerm = function() {
+        if (termIndex >= $scope.settings.terms.length) termIndex=0;
+        currentTerm = $scope.settings.terms[termIndex];
+    }
+
+    $scope.receiveTerms = function(terms) {
+        $scope.settings.terms = terms;
+        $scope.nextTerm();
+        $scope.$apply();
+    }
+
+    $scope.getTargetTerms = function() {
+        var targetTerms = new Array(currentTerm);
+        return targetTerms.shift();
+    }
+
+    $scope.getNativeTerm = function() {
+        return currentTerm[0];
+    }
+
+        $scope.getCorrect = function() {
+            if (! $scope.recognizedSpeech) return "";
+            if (! $scope.getNativeTerm()) return "";
+            if (! $scope.getTargetTerms()) return "";
+            var correct = false;
+            for (var t=0; t<$scope.getTargetTerms().length; t++) {
+                for (var s=0; s<$scope.recognizedSpeech.length; s++) {
+                    if ($scope.getTargetTerms().equals($scope.recognizedSpeech.toLowerCase())) {
+                        correct = true;
+                        break;
+                    }
+                }
+            }
+            if (correct) return "correct";
+            return "incorrect";
+        }
+
+    Lookup.getTerms($scope.receiveTerms);
+
 })
 
 .controller('InfoCtrl', function($scope, AndroidSpeechRecognizer, Settings) {
--- www/js/services.js
+++ www/js/services.js
@@ -3,19 +3,106 @@ angular.module('gabi.services', [])
 .factory('Settings', function() {
 
     return {
-        targetLanguage: 'es-ES'
+        targetLanguage: 'es-ES',
+
+        terms: []
     }
 })
 
 .factory('Lookup', ['$http', function($http){
     var url   = "lookup/en-US_es-ES.csv";
-    var terms = $http.get(url).then(function(response){
-        return csvParser(response.data);
-    });
-    return {
-        getTerms: function() {
-            return terms;
+    var terms = new Array();
+
+    function csvToArray( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+            (
+                // Delimiters.
+                "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                    // Quoted fields.
+                    "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                    // Standard fields.
+                    "([^\"\\" + strDelimiter + "\\r\\n]*))"
+                ),
+            "gi"
+        );
+
+
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+
+
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+
+            // Get the delimiter that was found.
+            var strMatchedDelimiter = arrMatches[ 1 ];
+
+            // Check to see if the given delimiter has a length
+            // (is not the start of string) and if it matches
+            // field delimiter. If id does not, then we know
+            // that this delimiter is a row delimiter.
+            if (
+                strMatchedDelimiter.length &&
+                    (strMatchedDelimiter != strDelimiter)
+                ){
+
+                // Since we have reached a new row of data,
+                // add an empty row to our data array.
+                arrData.push( [] );
+
+            }
+
+
+            // Now that we have our delimiter out of the way,
+            // let's check to see which kind of value we
+            // captured (quoted or unquoted).
+            if (arrMatches[ 2 ]){
+
+                // We found a quoted value. When we capture
+                // this value, unescape any double quotes.
+                var strMatchedValue = arrMatches[ 2 ].replace(
+                    new RegExp( "\"\"", "g" ),
+                    "\""
+                );
+
+            } else {
+
+                // We found a non-quoted value.
+                var strMatchedValue = arrMatches[ 3 ];
+
+            }
+
+
+            // Now that we have our value string, let's add
+            // it to the data array.
+            arrData[ arrData.length - 1 ].push( strMatchedValue );
         }
+
+        // Return the parsed data.
+        return( arrData );
+    }
+
+    return {
+        getTerms: function(callback) {
+            $http.get(url).then(function(response){
+            terms = response.data;
+            alert('read file: ' + response.data);
+            callback(csvToArray(response.data, ";"));
+        });
+      }
     }
 }])
 
@@ -46,7 +133,7 @@ angular.module('gabi.services', [])
 
     recognizeSpeech: function(callback, language) {
         var maxMatches = 5;
-        var promptString = "Speak now"; // optional
+        var promptString = "Say it in " + language; // optional
         if (window.plugins && window.plugins.speechrecognizer ) {
             window.plugins.speechrecognizer.startRecognize(function(result){
                 callback(result);
@@ -58,4 +145,13 @@ angular.module('gabi.services', [])
         }
     }
   }
+})
+
+
+    .factory('Util', function() {
+
+        return {
+
+
+}
 });
